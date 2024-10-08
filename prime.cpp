961c14aee38699182ba07f84db56f04ffcf50dd7
Muhammad Ramzan <kali.arch.78@gmail.com>
2024-10-08 07:03:47 +0000

    Add files via upload
    An easy and accurate way to find a number is prime or non-prime.

---
+++ prime.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    bool prime = true;
+
+    if(num==0 || num==1)
+    {
+        cout<<"Non-Prime Number"<<endl;
+    }
+    else if(num<0)
+    {
+        cout<<"Non-Prime Number"<<endl;
+    }
+    else
+    {
+
+       for(int i=2; i<=num-1; i++)
+    //  for(int i=2; i*i<=num; i++)                           //this trick is also useful and more efficient
+       {
+         if(num%i == 0)
+          {
+             prime = false;
+             break;
+          }
+       }
+
+       if(prime == true)                                     //one way to find a prime number
+        {
+            cout<<"Prime Number";
+        }
+      else
+        {
+            cout<<"Non-Prime Number";
+        }
+
+    }
+
+
+
+
+   return 0;
+}
