9e6607c225bfbfccfcaffdac9f5466ccd69820d7
Muhammad Ramzan <kali.arch.78@gmail.com>
2024-10-13 02:46:33 +0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,28 +1,46 @@
 #include<iostream>
-#include<bits/stdc++.h>
+#include<cstdlib>
 using namespace std;
 int main()
 {
 
-    int num = rand()%100+8;             //here 100 shows that the number will be between 0 and 100.
-    cout<<Enter your guess: ;
+    srand(time(0));                                  //srand(time(0)) will change seed value every second and gives us different guesses every single time.
+    int num = rand()%100;                            //here 100 shows that the number will be between 0 and 99.
+
+    int attempts = 1;
+    cout<<"Your attempt starts with 1 "<<endl;
+
+    int max;
+    cout<<"Enter maximum attempts you want to try: ";
+    cin>>max;
+
+    cout<<"Enter your guess: ";
     int guess;
     cin>>guess;
 
-    while(guess!=num)
-    {
+    while(guess != num && attempts < max)
+       {
+
         if(guess>num)
         {
-          cout<<Higher Guess<<endl;
+          cout<<"Higher Guess⤴️"<<endl;
         }
-        else
-        {
-            cout<<Lower Guess<<endl;
-        }
-       cin>>guess;
-    }
-    cout<<Accurate Guess <<endl;
+         else
+         {
+             cout<<"Lower Guess⤵️"<<endl;
+         }
+         cin>>guess;
+         attempts++;
 
-    return 0;
+       }
+
+         if(guess == num)
+          {
+            cout<<"Accurate guess😇"<<endl;
+          }
+         else
+          {
+            cout<<"Your attempts expired😭"<<endl;
+          }
+  return 0;
 }
-
